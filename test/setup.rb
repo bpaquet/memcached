7696ee7b3d0bf2d3518b990813db56b7e5d1840e
Bertrand Paquet <bpaquet@octo.com>
2013-08-02 20:08:32 +0200

    Extract a spawn_memcached function to allow more tests

--- test/setup.rb
+++ test/setup.rb
@@ -1,4 +1,10 @@
 
+def spawn_memcached options
+  pid = spawn("#{MEMCACHED_COMMAND} #{MEMCACHED_VERBOSITY} #{options} >> #{MEMCACHED_LOG} 2>&1")
+  raise "Unable to start memcached with options #{options}" if Process.waitpid(pid, Process::WNOHANG)
+  pid
+end
+
 unless defined? UNIX_SOCKET_NAME
   HERE = File.dirname(__FILE__)
   UNIX_SOCKET_NAME = File.join(ENV['TMPDIR']||'/tmp','memcached')
@@ -7,24 +13,22 @@ unless defined? UNIX_SOCKET_NAME
   system("killall -9 memcached")
 
   # Start memcached
-  verbosity = (ENV['DEBUG'] ? "-vv" : "")
-  log = "/tmp/memcached.log"
-  memcached = ENV['MEMCACHED_COMMAND'] || 'memcached'
-  system ">#{log}"
+  MEMCACHED_VERBOSITY = (ENV['DEBUG'] ? "-vv" : "")
+  MEMCACHED_LOG = "/tmp/memcached.log"
+  MEMCACHED_COMMAND = ENV['MEMCACHED_COMMAND'] || 'memcached'
+
+  system ">#{MEMCACHED_LOG}"
 
   # TCP memcached
   (43042..43046).each do |port|
-    cmd = "#{memcached} #{verbosity} -U 0 -p #{port} >> #{log} 2>&1 &"
-    raise "'#{cmd}' failed to start" unless system(cmd)
+    spawn_memcached "-U 0 -p #{port}"
   end
   # UDP memcached
   (43052..43053).each do |port|
-    cmd = "#{memcached} #{verbosity} -U #{port} -p 0 >> #{log} 2>&1 &"
-    raise "'#{cmd}' failed to start" unless system(cmd)
+    spawn_memcached "-U #{port} -p 0 "
   end
   # Domain socket memcached
   (0..1).each do |i|
-    cmd = "#{memcached} -M -s #{UNIX_SOCKET_NAME}#{i} #{verbosity} >> #{log} 2>&1 &"
-    raise "'#{cmd}' failed to start" unless system(cmd)
+    spawn_memcached " -M -s #{UNIX_SOCKET_NAME}#{i}"
   end
 end
